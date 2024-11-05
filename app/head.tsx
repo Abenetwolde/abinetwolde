0bbf909efe2a79a280c06267b3a8feb01fbab2a5
Abenetwolde <abnetwoldedev@gmail.com>
2024-11-05 16:38:16 +0300

    SEO

--- app/head.tsx
+++ app/head.tsx
@@ -2,23 +2,23 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta name="keywords" content="jigar sable, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
-      <meta property="og:site_name" content="Jigar Sable's Portfolio" />
-      <meta property="og:title" content="Portfolio | Jigar Sable - Full Stack Developer" />
+      <meta name="description" content="I'm Abnet Wolde, I am a Full-Stack developer based in Addis Ababa, Ethiopia. I'm passionate about creating beautiful, functional, and user-friendly Websites, Mobile Applications, and Telegram Bots. I'm constantly pushing myself to learn and grow as a developer. Love building full-stack Ethiopian clones and side projects." />
+      <meta name="keywords" content="abnet wolde, portfolio, abnet, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="abnet wolde's Portfolio" />
+      <meta property="og:title" content="Portfolio | Abnet Wolde - Full Stack Developer" />
       <meta property="og:description"
-        content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta property="og:url" content="https://jigarsable.vercel.app" />
-      <meta property="og:image" content="https://jigarsable.vercel.app/portfolio-fork.png" />
-      <link rel="canonical" href="https://jigarsable.vercel.app" />
-      <meta name="author" content="Jigar Sable" />
+        content="I'm Jigar Sable, full-stack developer based in Ethiopia, Addis Ababa. My journey in web development began during my first year of college." />
+      <meta property="og:url" content="https://abnet-wolde-dev.vercel.app/" />
+      <meta property="og:image" content="https://abnet-wolde-dev.vercel.app/landing.jpeg" />
+      <link rel="canonical" href="https://abnet-wolde-dev.vercel.app/" />
+      <meta name="author" content="Abnet Wolde" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://jigarsable.vercel.app" />
-      <meta name="application-name" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <meta name="apple-mobile-web-app-title" content="Portfolio | Jigar Sable - Full Stack Developer" />
+      <meta name="twitter:site" content="https://abnet-wolde-dev.vercel.app/" />
+      <meta name="application-name" content="Portfolio | Abnet Wolde - Full Stack Developer" />
+      <meta name="apple-mobile-web-app-title" content="Portfolio | Abnet Wolde - Full Stack Developer" />
       <link rel="icon" href="/favicon.ico" />
-      <title>Portfolio | Jigar Sable - Full Stack Developer</title>
+      <title>Portfolio | Abnet Wolde - Full Stack Developer</title>
     </>
   )
 }
