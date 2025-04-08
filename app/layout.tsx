466fee34072095f9fb3476b2d585815f3af953b9
Abenetwolde <abnetwoldedev@gmail.com>
2025-04-08 16:18:22 +0300

    add google ad sense

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head />
+    <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECDEKL29XG"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ECDEKL29XG');
+            `,
+          }}
+        />
+      </head>
       <ThemeProvider attribute='class' defaultTheme='light'>
         <body className={`${poppins.className} font-poppins bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden`}>
         <GoogleAnalytics gaId={"G-ECDEKL29XG"} />
